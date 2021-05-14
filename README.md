ebde1e9b6e3d5469c9c2b936510d35f7809e41a1
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2021-05-14 23:04:48 +0900

    各種APIの対応

--- README.md
+++ README.md
@@ -14,5 +14,18 @@ notionAPIをいじいじする
 ## テーブルにデータを追加
 `$  node index.js create_page {{table_Id}} {{追加する内容(テキスト)}}`
 ## テーブルの値をすべて取得する
-`$ node index.js query {{table_Id}}`
-
+カレンダーブロックにも使えた
+ボードにつかえた
+`$ node index.js scan {{table_Id}}`
+## テーブルの特定の項目を変更する
+`$ node index.js update {{table_Id}} {{page_Id}}`
+## dbからプロパティと項目が一致をクエリ
+`$ node index.js db_query {{table_Id}} {{property_name}} {{検索したい値(テキスト)}}`
+## ページの詳細情報を取得する
+`$ node index.js pages_retrieve {{parent_Id}}`
+## ページの中のブロックのリストを取得（ページのコンテンツ取得）
+`$ node index.js block_children {{parent_Id}}`
+## ページに子ブロックを追加する
+`$ node index.js append_block_children {{parent_Id}}`
+##　ボートからクエリする
+``
--- index.js
+++ index.js
@@ -17,8 +17,22 @@ async function main() {
     case "create_page":
       await createPageFunc();
       break;
-    case "query":
-      await queryFunc();
+    case "scan":
+      await scanFunc();
+      break;
+    case "db_query":
+      await dbQueryFunc();
+      break;
+    case "update":
+      await update();
+    case "pages_retrieve":
+      await pagesRetrieve();
+      break;
+    case "block_children":
+      await blockChildrenList();
+      break;
+    case "append_block_children":
+      await appendBlockChildren();
       break;
   }
 }
@@ -56,12 +70,95 @@ async function createPageFunc() {
     },
   });
 }
-async function queryFunc() {
+async function scanFunc() {
   //条件を指定せず全て取得する
   const request_payload = {
     path: "databases/" + process.argv[3] + "/query",
     method: "POST",
   };
   const current_pages = await notion.request(request_payload);
-  console.log(current_pages.results);
+  console.log(current_pages.results[2].properties.Status.select.name);
+}
+async function update() {
+  const request_payload = {
+    path: "pages/" + process.argv[3],
+    method: "patch",
+    body: {
+      parent: { database_id: process.argv[4] },
+      properties: {
+        Name: {
+          title: [
+            {
+              text: {
+                content: "新しいなまえ",
+              },
+            },
+          ],
+        },
+        "やること！": {
+          rich_text: [
+            {
+              text: {
+                content: "やらなければいけない",
+              },
+            },
+          ],
+        },
+      },
+    },
+  };
+  await notion.request(request_payload);
+}
+async function pagesRetrieve() {
+  const parentPage = await notion.pages.retrieve({
+    page_id: process.argv[3],
+  });
+  console.dir(parentPage.properties.title.title);
+}
+async function blockChildrenList() {
+  const blocks = await notion.blocks.children.list({
+    block_id: process.argv[3],
+  });
+  for (const block of blocks.results) {
+    console.dir(block);
+    console.dir(block.paragraph);
+  }
+}
+async function appendBlockChildren() {
+  const request_payload = {
+    path: "blocks/" + process.argv[3] + "/children",
+    method: "patch",
+    body: {
+      children: [
+        {
+          object: "block",
+          type: "to_do",
+          to_do: {
+            text: [{ type: "text", text: { content: "お風呂あらい" } }],
+            checked: false,
+            children: [],
+          },
+        },
+      ],
+    },
+  };
+  const result = await notion.request(request_payload);
+  console.log(result);
+}
+async function dbQueryFunc() {
+  //条件を指定せず全て取得する
+  const request_payload = {
+    path: "databases/" + process.argv[3] + "/query",
+    method: "POST",
+    body: {
+      filter: {
+        property: process.argv[4],
+        text: {
+          equals: `${process.argv[5]}`,
+        },
+      },
+    },
+  };
+  const current_pages = await notion.request(request_payload);
+  console.dir(current_pages.results);
 }
